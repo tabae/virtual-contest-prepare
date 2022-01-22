d8a9f139ad11a4782a2b594e143ae26d354891ed
Ebata <dongurinao@gmail.com>
2022-01-22 21:09:36 +0900

    bugfix

--- virtual-contest-prepare.py
+++ virtual-contest-prepare.py
@@ -6,13 +6,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-if(len(sys.argv) >= 2):
-    if(sys.argv[1] == '--help' or sys.argv[1] == '-h'):
-        print('Usage: python3 ' + sys.argv[0] + ' [URL of virtual contest page]')
-        sys.exit(0)
-    else:
-        out_dir = sys.argv[1]
-
+if(len(sys.argv) < 2 or sys.argv[1] == '--help' or sys.argv[1] == '-h'):
+    print('Usage: python3 ' + sys.argv[0] + ' [URL of virtual contest page]')
+    sys.exit(0)        
+    
 options = Options()
 options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
